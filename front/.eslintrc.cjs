3a3209287f3ec3d71667fae0874ee5180d617863
Fabrice G <fabou291@gmail.com>
2023-02-26 16:06:50 +0100

    🔧 update configuration files about linters

--- front/.eslintrc.cjs
+++ front/.eslintrc.cjs
@@ -1,14 +1,24 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require("@rushstack/eslint-patch/modern-module-resolution");
+const path = require("node:path");
+const createAliasSetting = require("@vue/eslint-config-airbnb/createAliasSetting");
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-prettier'
+  extends: [
+    "plugin:vue/vue3-essential",
+    "@vue/eslint-config-airbnb",
+    "@vue/eslint-config-prettier",
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: "latest",
+  },
+  rules: {
+    "import/no-unresolved": "error",
+  },
+  settings: {
+    ...createAliasSetting({
+      "@": `${path.resolve(__dirname, "./src")}`,
+    }),
+  },
+};
