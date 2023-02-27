d7d7ebe2e0d0624045c1f480390160fee7a8768b
Fabrice G <fabou291@gmail.com>
2023-02-27 22:44:39 +0100

    🔨 Implement HeaderComponent

---
+++ front/src/components/HeaderComponent.vue
@@ -0,0 +1,65 @@
+<script setup></script>
+
+<template>
+  <header class="header">
+    <div class="container">
+      <img class="header_brand" src="" alt="" />
+
+      <nav>
+        <ul>
+          <li><a href="#" class="active">Accueil</a></li>
+          <li><a href="#" class="notification">Réviser</a></li>
+          <li><a href="#">Live</a></li>
+          <li><a href="#">Explorer</a></li>
+          <li><a href="#">Tarifs</a></li>
+        </ul>
+      </nav>
+
+      <ul class="ml-auto">
+        <li><a href="#">Aide</a></li>
+        <li><a href="#" class="caret">user.name</a></li>
+      </ul>
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.header {
+  height: 60px;
+  border-bottom: 1px solid red;
+
+  &::after {
+    box-shadow: 0 2px 2px 0 #284b7c14;
+    content: "";
+    height: 1px;
+    left: 0;
+    position: absolute;
+    top: calc(3.75em - 1px);
+    width: 100%;
+  }
+
+  &_brand {
+    width: 92px;
+    margin-right: 30px;
+  }
+
+  .container {
+    padding: 0;
+    height: 100%;
+    display: flex;
+    align-items: center;
+  }
+
+  a.active {
+    color: var(--color-text-active);
+  }
+
+  li {
+    display: inline;
+  }
+
+  li:not(:last-child) {
+    margin-right: 24px;
+  }
+}
+</style>
--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,5 +1,31 @@
-<script setup></script>
+<script setup>
+import Header from "@/components/HeaderComponent.vue";
+import Hero from "@/views/HeroView.vue";
+import Feeds from "@/components/FeedsComponent.vue";
+</script>
 
 <template>
-  <main></main>
+  <Header />
+  <main>
+    <Hero />
+    <div class="container">
+      <section style="width: 776px">
+        <Feeds />
+      </section>
+      <!-- 
+      <aside>
+        <div></div>
+        <div>
+          <p>Débutant (A2) - Cours 1</p>
+          <p>6/7 leçons terminées</p>
+        </div>
+        <p>
+          Parler des sentiments et des relations, parler de ses projets, raconter
+          son week-end, prévoir ses vacances
+        </p>
+        <button>Essayer un autre cours</button>
+        <button>Vérifier votre niveau</button>
+      </aside> -->
+    </div>
+  </main>
 </template>
